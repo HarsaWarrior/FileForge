18b4387ebe6ec35e551b6ec81a706e4fede68c36
Harsh Patil <harsh3152002@gmail.com>
2025-07-22 18:18:22 +0530

    Fixed Sign In and Sign Up page display

--- file-forge/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ file-forge/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="p-6 bg-white rounded-xl shadow-lg">
+        <SignIn />
+      </div>
+    </div>
+  )
 }
--- file-forge/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ file-forge/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="p-6 bg-white rounded-xl shadow-lg">
+        <SignUp />
+      </div>
+    </div>
+  )
 }
